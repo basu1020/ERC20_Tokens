c6f9ef5c51a94aeb6831e51130a2de3917e57eae
basu1020 <bvp1020mu@gmail.com>
2022-07-19 19:59:47 +0530

    "test cases"

--- SnapShot_token/contracts/SnapshotToken.sol
+++ SnapShot_token/contracts/SnapshotToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts@4.6.0/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@4.6.0/token/ERC20/extensions/ERC20Snapshot.sol";
-import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, ERC20Snapshot, Ownable {
     constructor() ERC20("MyToken", "MTK") {
--- SnapShot_token/test/sample-test.js
+++ SnapShot_token/test/sample-test.js
@@ -1,19 +1,40 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { isCallTrace } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Snapshot Token contract Deployment", function () {
+  let Token;
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
+  let SnapShotToken;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  beforeEach(async function() {
+    Token = await ethers.getContractFactory("MyToken");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners() // ethers.getSigners() returns an array with 20 signers (these are fake ones)
+    SnapShotToken = await Token.deploy()
+  })
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  describe("Deployment", async function() {
+    it("Should set the right", async() => {
+      expect(await SnapShotToken.owner()).to.equal(owner.address);
+    })
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    it("Should set the Total Supply of tokens to the owner", async() => {
+      expect(await SnapShotToken.balanceOf(owner.address)).to.equal(10000000000000000000000n)
+    })
+  })
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
+  describe("Transaction", async() => {
+    it("Should be able to transfer tokens between accounts", async function(){
+      await SnapShotToken.transfer(addr1.address, 100)
+      expect(await SnapShotToken.balanceOf(addr1.address)).to.equal(100)
+
+      await SnapShotToken.connect(addr1).transfer(addr2.address, 50)
+      expect(await SnapShotToken.balanceOf(addr2.address)).to.equal(50)
+      expect(await SnapShotToken.balanceOf(addr1.address)).to.equal(50)
+    })
+
+  })
 });
