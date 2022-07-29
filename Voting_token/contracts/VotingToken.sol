830c2a14a170bd0af1a8e1c40e3681b7ad08b209
basu1020 <bvp1020mu@gmail.com>
2022-07-29 22:14:33 +0530

    added test cases of Voting Token

--- Voting_token/contracts/VotingToken.sol
+++ Voting_token/contracts/VotingToken.sol
@@ -2,12 +2,32 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract VotingToken is ERC20Votes, Ownable {
+contract VotingToken is ERC20, ERC20Votes, Ownable {
     constructor() ERC20("VotingToken", "VTK") ERC20Permit("VotingToken") {
         _mint(_msgSender(), 10000 * 10 ** decimals());
     }
 
-    // It is inheriting rest of the functions from ERC20Votes.    
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }    
 }
